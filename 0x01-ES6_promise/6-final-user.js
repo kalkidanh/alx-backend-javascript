721d4ef684534a4ef99099b4dd6daa1d2252077a
kalkidanh <kalkidanh89@gmail.com>
2023-07-26 18:27:56 +0000

    array returned

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,21 @@
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  const users = [
+    signUpUser(firstName, lastName),
+    uploadPhoto(fileName),
+  ];
+
+  return Promise.allSettled(users).then((values) => {
+    const arrayResult = [];
+    values.forEach((value) => {
+      if (value.status === 'fulfilled') {
+        arrayResult.push(value);
+      } else {
+        arrayResult.push({
+          status: value.status,
+          value: `Error: ${value.reason.message}`,
+        });
+      }
+    });
+    return arrayResult;
+  });
+}
