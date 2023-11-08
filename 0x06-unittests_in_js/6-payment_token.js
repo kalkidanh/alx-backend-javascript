e2f01527b5b8b8229ba2b57ddb141c1f5cd414e0
Kal Tollosa <106757309+kalkidanh@users.noreply.github.com>
2023-11-08 20:20:07 +0300

    Delete 0x06-unittests_in_js directory

--- 0x06-unittests_in_js/6-payment_token.js
+++
@@ -1,9 +0,0 @@
-const getPaymentTokenFromAPI = (success) => {
-  if (success) {
-    return new Promise((resolve, reject) => {
-      resolve({ data: 'Successful response from the API' })
-    });
-  }
-};
-
-module.exports = getPaymentTokenFromAPI;
